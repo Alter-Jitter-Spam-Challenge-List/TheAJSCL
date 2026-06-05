e59eee2329433bb1b26f0a662bf5c225a8aef37b
MiRPacK19 <168746407+MiRPacK19@users.noreply.github.com>
2026-06-06 00:02:39 +0300

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-20*Math.pow(rank-1, 0.533) + 300) *
+    let score = (300 - 41.5*Math.pow(rank-1, 0.4)) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
